1ecba63b97f6675b04dc2ae44a14c723a49a5f5a
sisisin <azsisisin@gmail.com>
2016-01-02 08:18:04 +0900

    add down migration

--- src/migrations/20160101202359-unnamed-migration.js
+++ src/migrations/20160101202359-unnamed-migration.js
@@ -17,12 +17,11 @@ module.exports = {
   },
 
   down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface.changeColumn('Checks', 'eventID', {
+      type: Sequelize.STRING
+    })
+    .then(function () {
+      return queryInterface.renameColumn('Checks', 'eventID', 'eventName');
+    });
   }
 };
